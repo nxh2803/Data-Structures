f816ac79d52edbe813b38159c944f69bbe66be55
Nguyễn Xuân Hùng <97716169+nxh2803@users.noreply.github.com>
2022-11-17 08:37:42 +0700

    Update file_mau.cpp

--- file_mau.cpp
+++ file_mau.cpp
@@ -81,7 +81,7 @@ template<class E>
 //     void print(){
 //         for(Node *v = head;v;v=v->next){cout<<v->element<<" ";}
 //         cout<<endl;
-//     }
+//     } 
 //     void push(const E & e){
 //         Node * v = new Node(e,head);
 //         head  = v;
@@ -225,7 +225,7 @@ template<class E>
 //         E element; 
 //         Node * left, *right;
 //         Node(E e = E(), Node * l = 0, Node * r = 0): element(e), left(l), right(r){}
-//         bool isLeaf() const {return (!left&&!right);}
+//         bool isLeaf() const {return (!left && !right);}
 //         bool isInternal() const{return (left||right);}
 //     };
 //     Node * root; 
@@ -234,7 +234,7 @@ template<class E>
 //             cout << "Khong tim thay\n"; 
 //             return r;
 //         } 
-//         if (r->element==e) return r; 
+//         if (r->element == e) return r; 
 //         if (e < r->element) return searchRec(r->left,e); 
 //         return searchRec(r->right, e); 
 //     }
@@ -284,15 +284,11 @@ template<class E>
 //         return 0; 
 //     }
 //     int leafRec(const Node * v) const{   // đếm nút lá
-//         if (v)
-//         {
-//             return v->isLeaf() + leafRec (v->left) + leafRec(v->right);
-//         }
+//         if (v){return v->isLeaf() + leafRec (v->left) + leafRec(v->right);}
 //         return 0; 
 //     }
 //     int heightRec(const Node * v) const{ // đếm chiều cao của một nút, tương đương chiều cao của cây nhận nút đó làm gốc
-//         if (v)
-//         {
+//         if (v){
 //             if (v->isLeaf()) return 0;
 //             return 1 + max (heightRec (v->left),heightRec(v->right));
 //         }
@@ -302,18 +298,21 @@ template<class E>
 //     BSTree(): root(0) {} 
 //     bool isEmpty() const {return root==0;}
 //     int getSize() const {return countNode();}
+// 
 //     void preOrder() const { preOrderRec(root); cout << endl; }
 //     void postOrder() const { postOrderRec(root);cout << endl; }
 //     void inOrder() const { inOrderRec(root); cout << endl;}
 //     int countNode() const {return nodeRec(root);}
+// 
 //     int countInternal() const {return internalRec(root);}
 //     int countSingleChild() const {return singleChildNodeRec(root);}
 //     int countLeaf() const {return leafRec(root);}
 //     int height() const {return heightRec(root); }
+// 
 //     Node * & search(const E & e){return searchRec(this->root, e); }
-//     virtual void add (const E& e) { addRec(root, e);} 
+//     void add (const E & e) { addRec(root, e);} 
 //     void clear() {clearRec(root);}
-//     virtual ~BSTree() {clear();}
+//     ~BSTree() {clear();}
 // }; 
 // int main(){
 //     BSTree<int> t; 
@@ -325,5 +324,3 @@ template<class E>
 //     t.inOrder();
 //     t.clear(); t.inOrder(); 
 // }
-
-
